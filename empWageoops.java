ee950b405cb87657474d977d943e421c3ed85bc2
daminisalwe1234 <salwedamini2gmail.com>
2020-05-11 15:00:23 +0530

    day19

---
+++ empWageoops.java
@@ -0,0 +1,12 @@
+public class empWageoops
+{
+	public static void main(String[] args)
+	{
+		int fulltime=1;
+		double empCheck=Math.floor(Math.random()*10)%2;
+	if (empCheck == fulltime)
+		System.out.println("emp is present");
+	else
+		System.out.println("emp is absent");
+	}
+}
