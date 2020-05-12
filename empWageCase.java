e4c66ccc78ad34aa77ef43c2869109b9aa47e355
daminisalwe1234 <salwedamini2gmail.com>
2020-05-13 04:35:39 +0530

    emp wageuc3

---
+++ empWageCase.java
@@ -0,0 +1,27 @@
+public class empWageCase {
+	void empSalary() {
+	int isPartTime = 2;
+	int empRatePrHr = 20;
+	int empr = 0;
+	int perDaySalary = 0;
+	int monthWorkingDay = 20;
+	int monthlySalary = 0;
+	for (int i = 1;i <=monthWorkingDay:i++)
+	{
+		double empCheck = Math.floor(Math.random() *10) % 2:
+		switch ((int)empCheck)
+		{
+			case isFullTime :
+				empHr = 8;
+				break;
+			case isFullTime :
+                        	empHr = 4;
+                        	break;
+		}
+		perDaySalary = empRateprHr * empHr;
+		monthlySalary = perDaySalary +montlySalary;
+	}
+	System.out.println("monthly Salary "+monthlySalary;
+}
+
+
